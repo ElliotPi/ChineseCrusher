4344158fb26dcef06a45dee5542e6289f22299c7
ElliotPi <elliot.lihsienpi@gmail.com>
2024-10-26 22:49:27 -0600

    Updated

---
+++ ChineseCrusher/View/Screens/FlashcardView.swift
@@ -0,0 +1,18 @@
+//
+//  Flashcard.swift
+//  ChineseCrusher
+//
+//  Created by IS 543 on 10/26/24.
+//
+
+import SwiftUI
+
+struct FlashcardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashcardView()
+}
---
+++ ChineseCrusher/View/Screens/TopicView.swift
@@ -0,0 +1,8 @@
+//
+//  TopicView.swift
+//  ChineseCrusher
+//
+//  Created by IS 543 on 10/26/24.
+//
+
+import Foundation
